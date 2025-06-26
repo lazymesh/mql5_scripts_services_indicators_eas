b2d0457307ff0b22e607e0dc896b340a6ac18c86
Ramesh Maharjan <ramesh.anahcolus@gmail.com>
2025-06-26 16:25:17 +0545

    multi threading introduced in simple socket

--- pythonTests/simple_socket_test.py
+++ pythonTests/simple_socket_test.py
@@ -1,15 +1,27 @@
 import socket
+import threading
 
-host = "localhost"
-port = 9070
+server = "localhost"
 
-if __name__ == "__main__":
+def sockInit():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(20)
-    text = str(input("mt5 or something else: "))
-    if sock.connect_ex((host, port)) != 0 :
+    return sock
+        
+def connectAndSend(sock, host, port):
+    if sock.connect_ex((host, port)) != 0:
         sock.close()
-    else:
-        while(len(text) > 0):
-            sock.send(text.encode('ascii'))
-            text = str(input("enter something : "))
+    strInput = "write something to send to the server on port {} : ".format(port)
+    text = str(input(strInput))
+    while(text != "close"):
+        sock.send(text.encode('ascii'))
+        text = str(input("type 'close' to close the connection "))
+    sock.close()
+    
+    
+if __name__ == "__main__":
+    port = input("enter a port to connect to server : ")
+    sock = sockInit()
+    thread = threading.Thread(target=connectAndSend, args=(sock, server, int(port)))
+    thread.start()
+    
