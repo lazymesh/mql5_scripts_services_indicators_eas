6a542677a366ac4f20f54ddb6ef829cbe6544793
Ramesh Maharjan <ramesh.anahcolus@gmail.com>
2025-09-03 18:37:13 +0545

    commented the listing

--- pythonTests/telegram/pyrogram/pyrogram_test.py
+++ pythonTests/telegram/pyrogram/pyrogram_test.py
@@ -29,7 +29,7 @@ def forward_to_bridge(client, message):
     print(f"Forwarded: {message.text}")
 
 if __name__ == "__main__":
-    list_joined_channels()
+    #list_joined_channels()
     if app.is_connected:
         print("app is already conneced")
     else:
